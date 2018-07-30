79384e16c5b84935ce4da3ba2a9b97fdd14677b0
Vitor José <vitorjsls30@gmail.com>
2018-07-30 17:04:19 -0300

    spf-01 - Adding base markup

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /|.js$/,
-        exclude: [/node_modules/],
-        use: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
           loader: 'babel-loader',
-        }]
+        }
       }
     ]
   }
