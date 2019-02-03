89a7e6a6ae7c245df05055e4c89bfcd0e1fde00b
Vitor José <vitorjsls30@gmail.com>
2019-02-03 19:13:49 -0200

    swp-01 - Adding AlbumList component / Adding AlbumList tests / Adjusting eslint options

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'vs-spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+    token: 'your_token',
+});
+
+export default spotify;
---
+++ tests/Spotify.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it  */
+import { expect } from 'chai';
+import spotify from '../src/Spotify';
+
+describe('Spotify', () => {
+  it('should be an object', () => {
+    expect(spotify).to.be.an('object');
+  });
+
+  it('should have search method', () => {
+    expect(spotify.search).to.exist;
+  });
+
+  it('should have album methods', () => {
+    expect(spotify.album).to.exist;
+  });
+});
