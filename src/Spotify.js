28ec7273af1c82a6beb057b3ba6238abe2aa49a0
Vitor José <vitorjsls30@gmail.com>
2019-02-03 19:44:59 -0200

    swp-01 - Adding renderAlbums method into main.js file

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'vs-spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-    token: 'your_token',
+    token: 'BQAoJX0A176lNkGOLoXCEALe_IKrEPAqLyKpby6twy1VUfH0CfaKkRXuRfFuYx9lLFTIZb3EFq_PuSDQEsC-BQl7a5XngL1ibcozE8UghY05jn0Xhb_ryTxcRcapjnfVilXCKjCuiG5USmcO',
 });
 
 export default spotify;
