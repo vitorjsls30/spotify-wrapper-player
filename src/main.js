468da82c7c456ad988ecd95c4f43c7b468171abb
Vitor José <vitorjsls30@gmail.com>
2019-03-11 17:12:11 -0300

    swp-01 - Adding SearchTrigger function into the solution

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 /* global document  */
 import spotify from './Spotify';
-import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
 import renderAlbumTracks from './AlbumTracks';
+import searchEnterTrigger from './SearchTrigger';
 
-const albums = spotify.search.albums('U2');
-const albumList = document.getElementById('album-list');
+searchEnterTrigger();
 
 const album = spotify.album.getAlbum('5y6wlw1LnqFnQFruMeiwGU');
 const albumInfo = document.getElementById('album-info');
 
 const albumTracks = document.getElementById('album-tracks');
-
-albums
-    .then(data => renderAlbums(data.albums.items, albumList));
 
 album
     .then((data) => {
